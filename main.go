862486951659312f57f04807482c836160e35c01
the-professor0-0 <abubakar.80187@gmail.com>
2022-04-27 00:51:25 +0500

    commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,31 +9,82 @@ import (
 	"path/filepath"
 )
 
+const (
+	ALBHABET_SIZE = 26
+)
+
+type trieNode struct {
+	childrens [ALBHABET_SIZE]*trieNode
+	isWordEnd bool
+}
+
+type trie struct {
+	root *trieNode
+}
+
+func initTrie() *trie {
+	return &trie{
+		root: &trieNode{},
+	}
+}
+
+func (t *trie) insert(word string) {
+	wordLength := len(word)
+	current := t.root
+	for i := 0; i < wordLength; i++ {
+		index := word[i] - 'a'
+		if current.childrens[index] == nil {
+			current.childrens[index] = &trieNode{}
+		}
+		current = current.childrens[index]
+	}
+	current.isWordEnd = true
+}
+
+func (t *trie) find(word string) bool {
+	wordLength := len(word)
+	current := t.root
+	for i := 0; i < wordLength; i++ {
+		index := word[i] - 'a'
+		if current.childrens[index] == nil {
+			return false
+		}
+		current = current.childrens[index]
+	}
+	if current.isWordEnd {
+		return true
+	}
+	return false
+}
+
 func main() {
 	files, err := ioutil.ReadDir("./files/")
 	if err != nil {
 		fmt.Println("ERROR FILE IS EMPTY")
 		log.Fatal(err)
 	}
-
+	lines := ""
 	for _, file := range files {
-		fmt.Println(file.Name(), filepath.Ext(file.Name()))
+		if filepath.Ext(file.Name()) == ".txt" {
+			fmt.Println(file.Name(), filepath.Ext(file.Name()))
+			_, error2 := os.Stat("./files/" + file.Name())
+
+			if errors.Is(error2, os.ErrNotExist) {
+				fmt.Println("file does not exist")
+			} else {
+				fmt.Println("file exists")
+			}
+
+			content, err := ioutil.ReadFile("./files/" + file.Name())
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			lines += string(content)
+		}
 	}
 
-	_, error := os.Stat("./files/")
-
-	// if errors.Is(err, os.ErrNotExist) {
-	// 	fmt.Println("file does not exist")
-	// } else {
-	// 	fmt.Println("file exists")
-	// }
-
-	content, err := ioutil.ReadFile("words.txt")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lines := string(content)
+	fmt.Println(lines)
 
 }
